999a7a8000c643db9eac87c53eefb1d783299543
Luke Steuber <luke@lukesteuber.com>
2026-03-22 20:09:14 -0500

    session checkpoint: 2026-03-22 20:09

--- swarm/swarm/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Swarm public package."""
-
-from .commands import COMMAND_SPECS, command_names, get_command
-
-__all__ = ["COMMAND_SPECS", "command_names", "get_command"]
-
-__version__ = "0.1.0"
